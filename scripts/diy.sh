f94523d5103bbd11e14d47306c15518a156d0e1b
baggio1024 <35676264+baggio1024@users.noreply.github.com>
2024-01-27 14:12:44 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #更改默认地址为192.168.6.1
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/10.5.1.1/10.5.7.1/g' package/base-files/files/bin/config_generate
 
